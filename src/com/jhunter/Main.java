e52212ded2345c3a878ebfaca8b0129d26eb50c9
jt <jitendra.chaudhary@gmail.com>
2021-11-30 15:35:42 -0700

    Given 3 sorted int arrays, merge them into one sorted array without duplicates.

--- src/com/jhunter/Main.java
+++ src/com/jhunter/Main.java
@@ -1,8 +1,16 @@
 package com.jhunter;
+import com.jhunter.fang.FANG2021;
+
+import static com.jhunter.fang.FANG2021.MergeMultipleSortedArray;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int[] arr1 = { 1,3,5};
+        int[] arr2 = { 2,3,6,9};
+        int[] arr3 = { 0,1,5,9};
+
+        int[] resArr = FANG2021.MergeMultipleSortedArray(arr1, arr2, arr3);
     }
 }
