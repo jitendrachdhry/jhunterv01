a4c19764b772f484e1fcc9f472a2a3376eab52cd
jt <jitendra.chaudhary@gmail.com>
2021-11-30 16:12:45 -0700

    Given 3 sorted int arrays, merge them into one sorted array without duplicates.

--- src/com/jhunter/Main.java
+++ src/com/jhunter/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int[] arr1 = { 1,3,5};
+        int[] arr1 = null; // { 1,3,5};
         int[] arr2 = { 2,3,6,9};
         int[] arr3 = { 0,1,5,9};
 
--- src/com/jhunter/fang/FANG2021.java
+++ src/com/jhunter/fang/FANG2021.java
@@ -1,4 +1,5 @@
 package com.jhunter.fang;
+
 import java.util.Arrays;
 
 public class FANG2021 {
@@ -17,7 +18,7 @@ public class FANG2021 {
 
         Doubts to confirm:
         1. Does all array have same size?
-        2. do we allow empty array as an input?
+        2. do we allow empty(null) array as an input?
 
         Approach:
         1. Create four index to carry counter for three input array and one result array.
@@ -36,19 +37,24 @@ public class FANG2021 {
 
          */
 
-        // write your code here
-        int size = arr1.length + arr2.length + arr3.length;
-        System.out.println("arr1.length: " + arr1.length + " arr2.length: " + arr2.length + " arr3.length: " + arr3.length);
+        // Calculate size of output array.
+        int size = (arr1 != null) ? arr1.length : 0;
+        size = (arr2 != null) ? arr2.length + size : size;
+        size = (arr3 != null) ? arr3.length + size : size;
 
         int[] resArr = new int[size];
         System.out.println("Result array length: " + resArr.length);
+
+        /* Initialize array indexes. */
         int idx1 = 0, idx2 = 0, idx3 = 0, resIdx = -1;
-        while (idx1 < arr1.length || idx2 < arr2.length || idx3 < arr3.length) {
+
+        /* Start a while loop to traverse all element of arr1, arr2 and arr3. */
+        while ((arr1 != null && idx1 < arr1.length) || (arr2 != null && idx2 < arr2.length) || (arr3 != null && idx3 < arr3.length)) {
             int minimumValueArr;
-            if (idx1 >= arr1.length ) { /* Check if we have traversed all element of arr1. */
-                if (idx2 >= arr2.length ) { /* Check if we have traversed all element of arr2. */
+            if (arr1 == null || idx1 >= arr1.length) { /* Check if we have traversed all element of arr1. */
+                if (arr2 == null || idx2 >= arr2.length) { /* Check if we have traversed all element of arr2. */
                     minimumValueArr = arr3[idx3++];
-                } else if (idx3 >= arr3.length ) { /* Check if we have traversed all element of arr3. */
+                } else if (arr3 == null || idx3 >= arr3.length) { /* Check if we have traversed all element of arr3. */
                     minimumValueArr = arr2[idx2++];
                 } else {
                     if (arr2[idx2] < arr3[idx3]) { /* Find minimum element from arr2 and arr3. */
@@ -57,8 +63,8 @@ public class FANG2021 {
                         minimumValueArr = arr3[idx3++];
                     }
                 }
-            } else if ( idx2 >= arr2.length  ) { /* Check if we have traversed all element of arr2. */
-                if (idx3 >= arr3.length ) { /* Check if we have traversed all element of arr3. */
+            } else if (arr2 == null || idx2 >= arr2.length) { /* Check if we have traversed all element of arr2. */
+                if (arr3 == null || idx3 >= arr3.length) { /* Check if we have traversed all element of arr3. */
                     minimumValueArr = arr1[idx1++];
                 } else { /* Find minimum element from arr1 and arr3. */
                     if (arr1[idx1] < arr3[idx3]) {
@@ -67,7 +73,7 @@ public class FANG2021 {
                         minimumValueArr = arr3[idx3++];
                     }
                 }
-            } else if ( idx3 >= arr3.length ) {/* Check if we have traversed all element of arr3. */
+            } else if (arr3 == null || idx3 >= arr3.length) {/* Check if we have traversed all element of arr3. */
                 if (arr1[idx1] < arr2[idx2]) { /* Find minimum element from arr1 and arr2. */
                     minimumValueArr = arr1[idx1++];
                 } else {
