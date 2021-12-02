b0a1f0827fb13c0c8eb148e5ab74028bbf0777f1
jt <jitendra.chaudhary@gmail.com>
2021-12-01 23:35:24 -0700

    Given array of integers as input and a target, return true if a contiguous subsequence sums to the target.

--- src/com/jhunter/fang/FANG2021DP.java
+++ src/com/jhunter/fang/FANG2021DP.java
@@ -1,7 +1,8 @@
 package com.jhunter.fang;
 
-import static java.lang.Math.max;
+import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicInteger;
+
 public class FANG2021DP {
     public static int addTwoStringNumberCompareWithOutputStringNumber(int[] inputArr, AtomicInteger startIdx, AtomicInteger endIdx) {
         /*
@@ -13,26 +14,28 @@ public class FANG2021DP {
         Result: 7 (2,3)
         */
 
-        if(inputArr == null ) {
-            startIdx.set(-1); endIdx.set(-1);
+        if (inputArr == null) {
+            startIdx.set(-1);
+            endIdx.set(-1);
             return -1;
         }
 
-        int [] dpArr = new int[inputArr.length];
+        int[] dpArr = new int[inputArr.length];
         dpArr[0] = inputArr[0];
-        int maxValue = inputArr[0], startJIdx , endJIdx;
-        startIdx.set(0); endIdx.set(0);
+        int maxValue = inputArr[0], startJIdx, endJIdx;
+        startIdx.set(0);
+        endIdx.set(0);
         startJIdx = endJIdx = 0;
-        for(int j=1; j<inputArr.length; j++) {
-            if(Math.max( ( dpArr[j-1] + inputArr[j] ) , inputArr[j] ) > inputArr[j] ) {
+        for (int j = 1; j < inputArr.length; j++) {
+            if (Math.max((dpArr[j - 1] + inputArr[j]), inputArr[j]) > inputArr[j]) {
                 endJIdx++;
-                dpArr[j] = dpArr[j-1] + inputArr[j];
+                dpArr[j] = dpArr[j - 1] + inputArr[j];
             } else {
                 startJIdx = endJIdx = j;
                 dpArr[j] = inputArr[j];
             }
 
-            if (dpArr[j] > maxValue){
+            if (dpArr[j] > maxValue) {
                 startIdx.set(startJIdx);
                 endIdx.set(endJIdx);
                 maxValue = dpArr[j];
@@ -41,5 +44,31 @@ public class FANG2021DP {
 
         System.out.println(" Start Idx: " + startIdx + " endIdx: " + endIdx + " Sum: " + maxValue);
         return maxValue;
+    }
+
+    public static boolean addTwoStringNumberCompareWithOutputStringNumber(int[] inputArr, int target) {
+        /*
+        Question:
+        Given array of integers as input and a target, return true if a contiguous subsequence sums to the target.
+        */
+
+        if (inputArr == null) {
+            return false;
+        }
+
+        boolean retValue = false;
+        int tail = 0, head = 1, sum = inputArr[0];
+        while (head < inputArr.length || sum >= target) {
+            if (sum < target) {
+                sum += inputArr[head++];
+            } else if (sum > target) {
+                sum -= inputArr[tail++];
+            } else {
+                retValue = true;
+                break;
+            }
+        }
+        System.out.println(" inputArr: " + Arrays.toString(inputArr) + " target: " + target + " found or not? " + retValue);
+        return retValue;
     }
 }
